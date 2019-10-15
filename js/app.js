f2b84cc95b6cf0093c37b7a5b7a11890db79feed
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2019-10-15 16:17:16 +0200

    GET/POST /konto, walidacja

--- js/app.js
+++ js/app.js
@@ -8,4 +8,20 @@ app1.controller("Ctrl1", [ "$http", function($http) {
         function (rep) { ctrl.konto = rep.data; },
         function (err) {}
     );
+    ctrl.transakcja = { operacja: "wy", kwota: 0 };
+    ctrl.message = '';
+    ctrl.robTransakcje = function() {
+        $http.post('/konto', ctrl.transakcja).then(
+            function (rep) { ctrl.konto = rep.data; ctrl.message = 'ok'; },
+            function (err) { console.log(err); ctrl.message = err.data.error; }    
+        );
+    };
+    ctrl.formInvalid = function() {
+        var mnoznik = 0;
+        switch(ctrl.transakcja.operacja) {
+            case 'wy': mnoznik = -1; break;
+            case 'wp': mnoznik = +1; break;
+        }
+        return ctrl.transakcja.kwota <= 0 || ctrl.konto.saldo + mnoznik * ctrl.transakcja.kwota < ctrl.konto.limit;
+    };
 }]);
--- server.js
+++ server.js
@@ -1,120 +1,70 @@
+/* internal modules */
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var mime = require('mime');
 
+/* own modules */
+var lib = require('./lib');
+
+/* configuration */
 var config = {
     port: 8888
 };
 
-var sendErrorOnStaticContent = function (response, code) {
-    console.log('>>> sending error ' + code + ' page');
-    response.writeHead(code, {'Content-Type': 'text/plain; charset=utf-8'});
-    switch (code) {
-        case 404:
-            response.write('Error 404: file not found.');
-            break;
-        case 403:
-            response.write('Error 403: access denied.');
-            break;
-        case 406:
-            response.write('Error 406: not acceptable');
-            break;
-        default:
-            response.write('Error ' + code);
-    }
-    response.end();
-}
-
-var sendJSONWithError = function (response, code, text) {
-    console.log('>>> sending JSON with error ' + code);
-    response.writeHead(code, 'ERROR', { 'Content-type': 'application/json'});
-    response.end(JSON.stringify({ error: text }));
-}
-
-var sendFile = function (response, filePath, fileContents) {
-    var mimeType = mime.getType(path.basename(filePath));
-    response.writeHead(200, {'Content-Type': mimeType });
-    console.log('>>> sending file ' + filePath + ' ' + mimeType);
-    response.end(fileContents);
-}
-
-var serveStaticContent = function (response, absPath) {
-    var n = absPath.indexOf('?');
-    var fileName = absPath.substring(0, n != -1 ? n : absPath.length);
-    fs.exists(fileName, function (exists) {
-        if (exists) {
-            fs.readFile(fileName, function (err, data) {
-                if (err) {
-                    sendErrorOnStaticContent(response, 406);
-                } else {
-                    sendFile(response, fileName, data);
-                }
-            });
-        } else {
-            if(fileName.endsWith('.map')) {
-                console.log('>>> skipping request for ' + absPath);
-                response.writeHead(200, {'Content-Type': 'application/json'});
-                response.end('{}');
-            } else {
-                sendErrorOnStaticContent(response, 404);
-            }
-        }
-    });
-}
-
+/* data */
 var konto = {
     saldo: 100,
     limit: -1000
 };
 
+/* HTTP server */
 var httpServer = http.createServer();
 
 httpServer.on('request', function (req, rep) {
-
     console.log('<<< ' + req.method + ' ' + req.url);
 
 	if(req.url == '/') {
-		serveStaticContent(rep, 'html/index.html');
+		lib.serveStaticContent(rep, 'html/index.html');
 	} else if(req.url == '/favicon.ico') {
-		serveStaticContent(rep, 'img/favicon.ico');
+		lib.serveStaticContent(rep, 'img/favicon.ico');
     } else if(/^\/(html|css|js|fonts|img)\//.test(req.url)) {
-        serveStaticContent(rep, '.' + req.url);
+        lib.serveStaticContent(rep, '.' + req.url);
     } else if(req.url == '/konto') {
         switch(req.method) {
             case 'GET':
-                rep.writeHead(200, 'OK', { 'Content-type': 'application/json' });
-                rep.end(JSON.stringify(konto));
+                lib.sendJSON(rep, konto);
                 break;
             case 'POST':
-                req.setEncoding('utf8');
-                var payload = '';
-                req.on('data', function(data) {
-                    payload += data;
-                }).on('end', function() {
-                    try {
-                        op = JSON.parse(payload);
-                        console.log(op);
-                        rep.writeHead(200, 'OK', { 'Content-type': 'application/json' });
-                        rep.end(JSON.stringify(konto));        
-                    } catch(ex) {
-                        sendJSONWithError(rep, 400, 'Payload is not valid JSON');
+                lib.payload2JSON(req, rep, function(req, rep, op, err) {
+                    if(err) {
+                        lib.sendJSONWithError(rep, 400, err.text);
+                    } else {
+                        var mnoznik = 0;
+                        switch(op.operacja) {
+                            case 'wy': mnoznik = -1; break;
+                            case 'wp': mnoznik = +1; break;
+                        }                
+                        if(konto.saldo + mnoznik * op.kwota < konto.limit) {
+                            lib.sendJSONWithError(rep, 400, 'Limit exceeded');
+                        } else {
+                            konto.saldo += mnoznik * op.kwota;
+                            lib.sendJSON(rep, konto);
+                        }
                     }
                 });
                 break;
             default:
-                sendJSONWithError(rep, 400, 'Invalid method for ' + req.url);
+                lib.sendJSONWithError(rep, 400, 'Invalid method ' + req.method + ' for ' + req.url);
         }
     } else {
-	    sendErrorOnStaticContent(rep, 403);
+	    lib.sendErrorOnStaticContent(rep, 403);
     }
-
 });
 
-try {
-    httpServer.listen(config.port);
-} catch(ex) {
-    console.log("Port " + config.port + " cannot be used");
-    process.exit();
-}
+/* main */
+
+process.on('uncaughtException', function(err) {
+    console.error('Runtime error ' + err.code + ' in the function \'' + err.syscall + '\'');
+    process.exit(1);
+});
+
+httpServer.listen(config.port);
 console.log("HTTP server is listening on the port " + config.port);
