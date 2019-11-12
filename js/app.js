f19334de106cdd050cefe30f1ee17626ec681f3c
Mariusz Jarocki <mariusz.jarocki@wmii.uni.lodz.pl>
2019-11-12 16:55:44 +0100

    Small fixes #2

--- js/app.js
+++ js/app.js
@@ -5,17 +5,22 @@ app1.controller("Ctrl1", [ "$http", function($http) {
     
     const defCreds = { email: 'jim@beam.com', password: 'admin1' };
 
-    ctrl.login = null;
-    ctrl.creds = defCreds;
-    ctrl.account = {};
-    ctrl.historyCount = 0;
-    ctrl.limit = 5;
-    ctrl.filter = '';
+    var initVars = function() {
+        ctrl.login = null;
+        ctrl.creds = defCreds;
+        ctrl.account = {};
+        ctrl.historyCount = 0;
+        ctrl.limit = 5;
+        ctrl.filter = '';
+        ctrl.transaction = { operation: "wi", amount: 0, description: "" };
+        ctrl.message = '';
+        $http.get('/login').then(
+            function(rep) { ctrl.login = rep.data.email; },
+            function(err) {}
+        );
+    };
 
-    $http.get('/login').then(
-        function(rep) { ctrl.login = rep.data.email; },
-        function(err) {}
-    );
+    initVars();
 
     ctrl.doLogin = function() {
         $http.post('/login', ctrl.creds).then(
@@ -32,9 +37,7 @@ app1.controller("Ctrl1", [ "$http", function($http) {
     ctrl.doLogout = function() {
         $http.delete('/login').then(
             function(rep) {
-                ctrl.login = null;
-                ctrl.transaction = { operation: "wi", amount: 0, description: '' },
-                ctrl.creds = defCreds;
+                initVars();
                 ctrl.message = 'Logout ok';
                 refreshAll();
             },
@@ -49,9 +52,6 @@ app1.controller("Ctrl1", [ "$http", function($http) {
         );
     };
 
-    ctrl.transaction = { operation: "wi", amount: 0, description: "" };
-    ctrl.message = '';
-    
     ctrl.doTransfer = function() {
         $http.post('/account', ctrl.transaction).then(
             function (rep) {
@@ -60,12 +60,19 @@ app1.controller("Ctrl1", [ "$http", function($http) {
                 if(ctrl.history.length >= ctrl.limit) {
                     ctrl.history.pop();
                 }
-                ctrl.history.unshift({ date: ctrl.account.lastOperation, operation: ctrl.transaction.operation, amount: ctrl.transaction.amount, description: ctrl.transaction.description, balance: ctrl.account.balance});
+                ctrl.history.unshift({
+                    date: ctrl.account.lastOperation,
+                    operation: ctrl.transaction.operation,
+                    amount: ctrl.transaction.amount,
+                    description: ctrl.transaction.description,
+                    balance: ctrl.account.balance
+                });
                 refreshHistoryCount();
             },
             function (err) { console.log(err); ctrl.message = err.data.error; }    
         );
     };
+
     ctrl.formInvalid = function() {
         var multiplier = 0;
         switch(ctrl.transaction.operation) {
@@ -74,27 +81,32 @@ app1.controller("Ctrl1", [ "$http", function($http) {
         }
         return ctrl.transaction.amount <= 0 || ctrl.account.balance + multiplier * ctrl.transaction.amount < ctrl.account.limit;
     };
+
     var refreshHistoryCount = function() {
         $http.delete('/history').then(
             function(rep) { ctrl.historyCount = rep.data.count; },
             function(err) {}
         );
     };
+
     var refreshAll = function() {
         refreshAccount();
         refreshHistoryCount();
         ctrl.refreshHistory();
     };
+
     ctrl.refreshHistory = function() {
         refreshHistoryCount();
         $http.get('/history?skip=0&limit=' + ctrl.limit + '&filter=' + ctrl.filter).then(
             function(rep) { ctrl.history = rep.data; },
             function(err) {}
         );
-    }
+    };
+
     ctrl.stamp2date = function(stamp) {
         return new Date(stamp).toLocaleString();
     };
+    
     ctrl.loadMore = function() {
         ctrl.limit += 5;
         if(ctrl.limit > ctrl.historyCount) ctrl.limit = ctrl.historyCount;
